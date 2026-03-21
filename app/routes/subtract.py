a174a1682b1dc06bb7b19ad71c88bc128437e757
Mitzy521 <159392464+Mitzy521@users.noreply.github.com>
2026-03-21 16:50:47 -0600

    subtract.py

--- app/routes/subtract.py
+++ app/routes/subtract.py
@@ -5,7 +5,7 @@ router = APIRouter()
 @router.get("/subtract")
 def calculate_subtract(num1: float, num2: float):
     return {
-        "operation": "sum",
+        "operation": "subtract",
         "num1": num1,
         "num2": num2,
         "result": num1 - num2
